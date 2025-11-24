bc1b209b76e4381839ae773d5971231af8d8d258
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-11-24 15:49:13 -0600

    musica en main menu

--- Assets/Scripts/escenas/MenuInicio.cs
+++ Assets/Scripts/escenas/MenuInicio.cs
@@ -3,6 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicio : MonoBehaviour
 {
+    // Arrastra tu archivo de audio aquí desde el inspector
+    public AudioClip musicaDeFondo;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Crear o usar un AudioSource en el mismo objeto
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = musicaDeFondo;
+        audioSource.loop = true; // Para que se repita
+        audioSource.playOnAwake = true; // Que empiece al iniciar
+        audioSource.volume = 0.5f; // Ajusta volumen a tu gusto
+        audioSource.Play();
+    }
+
     public void Jugar()
     {
         SceneManager.LoadScene("Gameplay");
